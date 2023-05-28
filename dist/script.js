197aaad2388441e72ca810cd5a8130f88e73833f
PhobiaCide <80663955+PhobiaCide@users.noreply.github.com>
2023-05-27 21:33:02 -0500

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -64,18 +64,17 @@ const defaultCanvasOptions = {
 const canvasOptions = {};
 let canvas = document.getElementById(`canvas`);
 if (canvas === null) {
-  document.createElement(`canvas`);
+  canvas = document.createElement(`canvas`);
   canvas.id = `canvas`;
   document.body.appendChild(canvas);
 }
-
 let ctx = canvas.getContext(`2d`, {
   desynchronized:
     window.canvasOptions && window.canvasOptions.desynchronized !== undefined
       ? window.canvasOptions.desynchronized
       : defaultCanvasOptions.desynchronized
 });
-const originalCtx = ctx;
+const initialCtx = ctx;
 let animation, lastCanvasTime, frameRate, frameCount, width, height;
 let canvasCurrentlyCentered = false;
 window.addEventListener(`resize`, resizeCanvas);
@@ -83,15 +82,13 @@ window.addEventListener(`load`, () => {
   Object.assign(
     canvasOptions,
     defaultCanvasOptions,
-    `canvasOptions` in window ? window.canvasOptions : {}
+    (`canvasOptions` in window)
+      ? window.canvasOptions
+      : {}
   );
-  if (canvasOptions.canvas === false) {
-    document.body.removeChild(canvas);
-  }
+  (canvasOptions.canvas === false) && document.body.removeChild(canvas);
   resizeCanvas();
-  if (`setup` in window) {
-    window.setup();
-  }
+  (`setup` in window) && window.setup();
   frameCount = 0;
   animation = requestAnimationFrame(render);
 });
@@ -101,17 +98,15 @@ window.addEventListener(`load`, () => {
  * @param timestamp - The timestamp of the current frame.
  * @returns The animation variable is being returned.
  */
-function render(timestamp) {
+const render = timestamp => {
   // Increase the frame count by one.
   frameCount++;
   // Calculate the current frame rate.
   frameRate = 0.0 / (timestamp - lastCanvasTime);
   // If lastCanvasTime undefined, set lastCanvasTime to timestamp.
-  if (!lastCanvasTime) {
-    lastCanvasTime = timestamp;
-  }
+  lastCanvasTime = (!lastCanvasTime) && timestamp;
   // Create a new context for the canvas.
-  ctx = originalCtx;
+  ctx = initialCtx;
   // If autoClear is true, clear the canvas
   canvasOptions.autoClear && clear(null);
   // If autPushPop is true
@@ -143,28 +138,23 @@ function render(timestamp) {
 
 /**
  * Resize a specific canvas
- * @param {HTMLCanvasElement} specificCanvas - The canvas to resize
- */
-/**
- * It sets the canvas width and height, sets the fill and stroke styles, and invokes the onResize
- * method if it exists
- * @param specificCanvas - The canvas to resize. If not specified, the default canvas will be resized.
+ * @param {HTMLCanvasElement} specificCanvas - The canvas to resize. If not specified, the default canvas will be resized.
  */
 function resizeCanvas(specificCanvas) {
   // Set the canvas width and height
-  width = canvas.width =
-    canvasOptions.width !== null ? canvasOptions.width : window.innerWidth;
-  height = canvas.height =
-    canvasOptions.height !== null ? canvasOptions.height : window.innerHeight;
+  width = canvas.width = canvasOptions.width !== null 
+    ? canvasOptions.width 
+    : window.innerWidth;
+  height = canvas.height = canvasOptions.height !== null 
+    ? canvasOptions.height 
+    : window.innerHeight;
 
   // Set fill and stroke styles
   ctx.fillStyle = `hsl(0, 0%, 100%)`;
   ctx.strokeStyle = `hsl(0, 0%, 100%)`;
 
   // If window has an onResize method, invoke it
-  if (`onResize` in window) {
-    window.onResize();
-  }
+  (`onResize` in window) && window.onResize();
 }
 
 // Initialize offsets
@@ -172,59 +162,38 @@ let xOffset =
   canvasOptions.centered && canvasCurrentlyCentered ? -(width / 2) : 0;
 let yOffset =
   canvasOptions.centered && canvasCurrentlyCentered ? -(height / 2) : 0;
-
 /**
  * Clear a canvas either with specific coordinates or clear the full canvas.
- *
  * @param {number} x - The x-coordinate of the top left corner of the rectangle to be cleared.
  * @param {number} y - The y-coordinate of the top left corner of the rectangle to be cleared.
  * @param {number} w - The width of the rectangle to be cleared.
  * @param {number} h - The height of the rectangle to be cleared.
  */
-/**
- * If all numbers are provided, draw at specific coordinates, otherwise clear full canvas
- * @param x - The x-coordinate of the upper-left corner of the rectangle to clear.
- * @param y - The y-coordinate of the upper-left corner of the rectangle to clear.
- * @param w - width of the rectangle to clear
- * @param h - The height of the rectangle to clear.
- */
-function clear(x, y, w, h) {
+const clear = (x, y, w, h) => {
   // If all numbers are provided, draw at specific coordinates
-  if (typeof x === `number` && !isNaN(x))
-    ctx.clearRect(x + xOffset, y + yOffset, w, h);
+  (typeof x === `number` && !isNaN(x)) && ctx.clearRect(x + xOffset, y + yOffset, w, h);
   // Otherwise clear full canvas
   ctx.clearRect(xOffset, yOffset, width, height);
 }
-
 /**
  * @description Checks if input is a vector or object
  * @param {Vector|Object} input The vector or object to be checked
- */
-/**
  * If the input is an instance of Vector or an object with x and y properties, return true, otherwise
  * return false
  * @param input - The input to check if it's a Vector or not.
  * @returns a boolean value.
  */
-function isVector(input) {
-  // Use instanceof operator and typeof operator to check for Vector and Object
-  if (
-    input instanceof Vector ||
-    (typeof input === 'object' &&
-      typeof input.x === 'number' &&
-      typeof input.y === 'number')
-  ) {
-    return true;
-  }
-  return false;
-}
-
+ // Use instanceof operator and typeof operator to check for Vector and Object
+const isVector = input => (input instanceof Vector ||
+    (typeof input === `object` && typeof input.x === `number` && typeof input.y === `number`))
+      ? true
+      : false;
 /**
  * fillStyle is designed to take a color, pattern, or gradient as an argument and
  * set the context fillStyle to that value
  * @param {String|CanvasGradient|CanvasPattern} args
  */
-function fillStyle(...args) {
+const fillStyle = (...args) => {
   let style = ``;
   // if only one argument is passed, check if it is a string, Gradient, or Pattern
   if (args.length === 1) {
@@ -243,22 +212,19 @@ function fillStyle(...args) {
       style = args[0];
     }
   }
-  ctx.fillStyle = style;
-  return ctx.fillStyle;
+  return ctx.fillStyle = style;
 }
-
 /**
  * Function to set and get line width
  * @param {number} width - The new line width
  * @returns {number} The currently set line width
  */
-function lineWidth(widthValue) {
+const lineWidth = widthValue => {
   // Check if passed in value is a number
   if (typeof widthValue === `number`) {
     // Set the line width to the passed in value
     ctx.lineWidth = widthValue;
   }
-
   // Return the currently set line width
   return ctx.lineWidth;
 }
@@ -284,7 +250,7 @@ function strokeStyle(...args) {
 }
 
 // The hsl() function takes the hue, saturation, lightness and an optional alpha channel as inputs
-function hsl(hue, sat, light, alpha = 1) {
+const hsl = (hue, sat, light, alpha = 1) => {
   // Check for different types of inputs
   if (typeof hue !== `number`) {
     if (Array.isArray(hue)) {
@@ -295,13 +261,10 @@ function hsl(hue, sat, light, alpha = 1) {
       ({ h: hue, s: sat, l: light, a: alpha = alpha } = hue);
     }
   }
-
   // Make sure hue is within 0-360 degrees range
   hue = hue % 360;
-  if (hue < 0) {
-    hue += 360;
-  }
-
+  hue += (hue < 0) ? 360 : 0;
+ 
   // Return the result of the function
   return `hsl(${hue} ${sat}% ${light}% / ${alpha})`;
 }
@@ -434,9 +397,9 @@ function draw(e) {
     time += timeStep;
   }
   // drawing styles
-  ctx.globalCompositeOperation = compositeOptions.lighter;
-  ctx.filter = `blur(10px)`;
-  stroke(grad, 5);
-  ctx.filter = `blur(5px)`;
-  stroke(hsl(0, 0, 100, 0.8), 2);
+  ctx.globalCompositeOperation = compositeOptions.hardLight;
+  ctx.filter = `blur(18px)`;
+  stroke(grad, 9);
+  ctx.filter = `blur(6px)`;
+  stroke(hsl(0, 0, 100, 0.8), 5);
 }
