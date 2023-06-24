33bac4abda09dfa1900866e2d48f167bb85b9d7d
PhobiaCide <80663955+PhobiaCide@users.noreply.github.com>
2023-06-24 11:19:05 +0000

    Jun 24, 2023, 6:19 AM

--- script.js
+++ script.js
@@ -310,7 +310,7 @@ function draw(e) {
   // incremental amount for cell coordinates
   const incrementX = strokeSegments == 1 ? 1 : 1 / (strokeSegments - 1);
 
-  const incrementY = strokeCount == 1 ? 1 : (strokeCount - 1) / 1;
+  const incrementY = (strokeCount - 1) / 1;
 
   // time step - used as frequency multiplier for noise
   let time = e * 0.00045;
